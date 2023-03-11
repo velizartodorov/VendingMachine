ec41729d8f42602ff49c05e1d1bde894ca1993db
Velizar Todorov <willking66@gmail.com>
2023-03-11 23:43:27 +0100

    :sparkle: add `Coin` enum

---
+++ src/main/kotlin/coin/Coin.kt
@@ -0,0 +1,12 @@
+package coin
+
+enum class Coin(val coin: String) {
+    ONE_CENT("1"),
+    TWO_CENTS("1"),
+    FIVE_CENTS("5"),
+    TEN_CENTS("10"),
+    TWENTY_CENTS("20"),
+    FIFTY_CENTS("50"),
+    ONE_EURO("1EU"),
+    TWO_EUROS("2EU"),
+}
