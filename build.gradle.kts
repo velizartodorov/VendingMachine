8ef358569a1777e7d69e734bdb5f0dd93e0e978a
Velizar Todorov <willking66@gmail.com>
2023-03-12 01:42:00 +0100

    :white_check_mark: add `OrderTest`

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
     testImplementation(kotlin("test"))
 }
 
---
+++ src/test/kotlin/order/OrderTest.kt
@@ -0,0 +1,26 @@
+package order
+
+import drinks.DrinkEnum.LATTE
+import order.Amount.MEDIUM
+import order.Amount.NONE
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class OrderTest {
+
+    @Test
+    fun testOrderLatte() {
+        val latte = Order.Builder()
+            .drink(LATTE)
+            .withMilk(NONE)
+            .withStrength(MEDIUM)
+            .withSugar(NONE)
+            .withPrice(30)
+            .order()
+        assertEquals(latte.drink, LATTE)
+        assertEquals(latte.milk, NONE)
+        assertEquals(latte.strength, MEDIUM)
+        assertEquals(latte.sugar, NONE)
+        assertEquals(latte.price, 30)
+    }
+}
