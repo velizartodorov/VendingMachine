a21fa450e26b9b1454025122b08a1fb563daee49
Velizar Todorov <willking66@gmail.com>
2023-03-13 02:10:56 +0100

    :sparkles: add change functionality

--- src/test/kotlin/order/OrderTest.kt
+++ src/test/kotlin/order/OrderTest.kt
@@ -3,6 +3,7 @@ package order
 import coin.Coin.FIFTY_CENTS
 import drinks.DrinkType.LATTE
 import order.Amount.NONE
+import order.Strength.MEDIUM
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -13,13 +14,13 @@ internal class OrderTest {
         val latte = Order.Builder()
             .drink(LATTE)
             .withMilk(NONE)
-            .withStrength(Strength.MEDIUM)
+            .withStrength(MEDIUM)
             .withSugar(NONE)
             .withCoins(FIFTY_CENTS)
             .build()
         assertEquals(latte.drink, LATTE)
         assertEquals(latte.milk, NONE)
-        assertEquals(latte.strength, Strength.MEDIUM)
+        assertEquals(latte.strength, MEDIUM)
         assertEquals(latte.sugar, NONE)
         assertEquals(latte.coins, FIFTY_CENTS)
     }
