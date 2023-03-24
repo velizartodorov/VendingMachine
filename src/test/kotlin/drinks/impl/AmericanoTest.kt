02084025cf7345c7f49034b210f724c61e5d611d
Velizar Todorov <willking66@gmail.com>
2023-03-24 02:18:33 +0100

    :white_check_mark: improve tests

--- src/test/kotlin/drinks/impl/AmericanoTest.kt
+++ src/test/kotlin/drinks/impl/AmericanoTest.kt
@@ -7,6 +7,7 @@ import order.Amount.NONE
 import order.Status.DONE
 import order.Strength.MEDIUM
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import vending_machine.TestHelper
 import vending_machine.VendingMachine
@@ -20,7 +21,7 @@ internal class AmericanoTest {
         assertEquals(americano.milk, NONE)
         assertEquals(americano.strength, MEDIUM)
         assertEquals(americano.sugar, NONE)
-        assertEquals(americano.coins, listOf(TWENTY_CENTS, TWENTY_CENTS, TWENTY_CENTS))
+        assertEquals(americano.coins, listOf(TWENTY_CENTS, TWENTY_CENTS, TWENTY_CENTS, TWENTY_CENTS))
     }
 
     @Test
@@ -28,8 +29,8 @@ internal class AmericanoTest {
         VendingMachine.start()
         val americano = TestHelper.getAmericano()
         val orderStatus = VendingMachine.prepare(americano)
-        Assertions.assertEquals(orderStatus.drink, AMERICANO)
-        Assertions.assertEquals(orderStatus.status, DONE)
-        Assertions.assertEquals(orderStatus.change, listOf(TEN_CENTS))
+        assertEquals(orderStatus.drink, AMERICANO)
+        assertEquals(orderStatus.status, DONE)
+        assertEquals(orderStatus.change, listOf(TWENTY_CENTS, TEN_CENTS))
     }
 }
