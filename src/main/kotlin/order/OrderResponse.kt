c850cca9c3e77526e391784cbd2b60820946a437
Velizar Todorov <willking66@gmail.com>
2023-03-24 03:03:53 +0100

    :hammer: refactoring `Drink` & `OrderResponse`

--- src/main/kotlin/order/OrderResponse.kt
+++ src/main/kotlin/order/OrderResponse.kt
@@ -4,8 +4,8 @@ import coin.Coin
 import drinks.DrinkType
 import order.Status.TO_DO
 
-class OrderResponse {
-    var change: List<Coin?> = emptyList()
-    var status: Status = TO_DO
-    lateinit var drink: DrinkType
-}
+data class OrderResponse(
+    var change: List<Coin> = listOf(),
+    var status: Status = TO_DO,
+    var drink: DrinkType,
+)
