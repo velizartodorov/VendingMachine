3063531de8bac362068fc95d28f3047c46e09d81
Velizar Todorov <willking66@gmail.com>
2023-04-07 15:52:13 +0200

    :bug: fix stack overflow error

--- src/main/kotlin/containers/TeaContainer.kt
+++ src/main/kotlin/containers/TeaContainer.kt
@@ -6,7 +6,7 @@ class TeaContainer : Container {
     override var amount = 500.0
 
     override fun extractAmount(amount: Amount) {
-        extractAmount(amount)
+        reduceAmount(amount)
         println("Tea extracted. Tea amount remaining: ${this.amount} gr")
     }
 }
