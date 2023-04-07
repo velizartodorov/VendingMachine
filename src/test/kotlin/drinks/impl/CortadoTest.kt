477edb145c2dafe3ab22f2f638b902fd78569d88
Velizar Todorov <willking66@gmail.com>
2023-04-07 18:14:11 +0200

    :white_check_mark: :hammer: remove TestHelper

--- src/test/kotlin/drinks/impl/CortadoTest.kt
+++ src/test/kotlin/drinks/impl/CortadoTest.kt
@@ -3,18 +3,19 @@ package drinks.impl
 import coin.Coin.FIFTY_CENTS
 import drinks.DrinkType.CORTADO
 import order.Amount.NONE
+import order.Order
 import order.Status.DONE
 import order.Strength.MEDIUM
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import vending_machine.TestHelper
 import vending_machine.VendingMachine
 
 internal class CortadoTest {
 
     @Test
     fun testOrderCortado() {
-        val drink = TestHelper.getCortado()
+        val drink = getCortado()
         assertEquals(drink.drink, CORTADO)
         assertEquals(drink.milk, NONE)
         assertEquals(drink.strength, MEDIUM)
@@ -25,11 +26,18 @@ internal class CortadoTest {
     @Test
     fun testVendingMachineWithCortado() {
         VendingMachine.start()
-        val drink = TestHelper.getCortado()
+        val drink = getCortado()
         val orderStatus = VendingMachine.prepare(drink)
         assertEquals(orderStatus.drink, CORTADO)
         assertEquals(orderStatus.status, DONE)
         assertTrue(orderStatus.change.isEmpty())
     }
 
+    private fun getCortado() = Order.Builder()
+        .drink(CORTADO)
+        .withMilk(NONE)
+        .withStrength(MEDIUM)
+        .withSugar(NONE)
+        .withCoins(FIFTY_CENTS)
+        .build()
 }
