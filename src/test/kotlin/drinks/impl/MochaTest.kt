e8a52f236a6b8adb30cf0ee1e0923a2cd8e665b2
Velizar Todorov <willking66@gmail.com>
2023-04-07 18:58:16 +0200

    :white_check_mark: add `MochaTest`

---
+++ src/test/kotlin/drinks/impl/MochaTest.kt
@@ -0,0 +1,42 @@
+package drinks.impl
+
+import coin.Coin.ONE_EURO
+import coin.Coin.TWENTY_CENTS
+import drinks.DrinkType.MOCHA
+import order.Amount.NONE
+import order.Order
+import order.Status.DONE
+import order.Strength.MEDIUM
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import vending_machine.VendingMachine
+
+internal class MochaTest {
+    @Test
+    fun testOrderMocha() {
+        val drink = getMocha()
+        assertEquals(drink.drink, MOCHA)
+        assertEquals(drink.milk, NONE)
+        assertEquals(drink.strength, MEDIUM)
+        assertEquals(drink.sugar, NONE)
+        assertEquals(drink.coins, listOf(ONE_EURO))
+    }
+
+    @Test
+    fun testVendingMachineWithMocha() {
+        VendingMachine.start()
+        val drink = getMocha()
+        val orderStatus = VendingMachine.prepare(drink)
+        assertEquals(orderStatus.drink, MOCHA)
+        assertEquals(orderStatus.status, DONE)
+        assertEquals(orderStatus.change, listOf(TWENTY_CENTS, TWENTY_CENTS))
+    }
+
+    private fun getMocha() = Order.Builder()
+        .drink(MOCHA)
+        .withMilk(NONE)
+        .withStrength(MEDIUM)
+        .withSugar(NONE)
+        .withCoins(ONE_EURO)
+        .build()
+}
