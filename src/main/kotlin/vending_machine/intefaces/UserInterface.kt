16c13813a61f57e5193919f7d7689e8019a57d5e
Velizar Todorov <willking66@gmail.com>
2023-05-03 03:39:43 +0200

    :hammer: shorter functions

--- src/main/kotlin/vending_machine/intefaces/UserInterface.kt
+++ src/main/kotlin/vending_machine/intefaces/UserInterface.kt
@@ -1,21 +1,18 @@
 package vending_machine.intefaces
 
 import order.Order
-import java.util.stream.Stream
 
 interface UserInterface {
     fun print()
     fun process(order: Order): Order
 
     companion object {
-        fun getAll(): Stream<UserInterface> {
-            return listOf(
-                DrinkInterface,
-                StrengthInterface,
-                MilkInterface,
-                SugarInterface,
-                CoinsInterface
-            ).stream()
-        }
+        fun getAll() = sequenceOf(
+            DrinkInterface,
+            StrengthInterface,
+            MilkInterface,
+            SugarInterface,
+            CoinsInterface
+        )
     }
 }
