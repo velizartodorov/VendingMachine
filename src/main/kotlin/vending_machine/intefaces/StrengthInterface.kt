8be6da2b999f3e48edf241238f8baafb254e782e
Velizar Todorov <willking66@gmail.com>
2023-11-08 23:24:30 +0100

    :art: enhance StrengthInterface

--- src/main/kotlin/vending_machine/intefaces/StrengthInterface.kt
+++ src/main/kotlin/vending_machine/intefaces/StrengthInterface.kt
@@ -7,23 +7,33 @@ object StrengthInterface : UserInterface {
 
     override fun print() {
         println(
+
             """
-    Strength LOW: 1
-    Strength MEDIUM: 2
-    Strength HIGH: 3
-    Strength MAX: 4
-    
-    ===================================
-    PICK A STRENGTH (1 - 4):
+==========================================================
+ / ___|  | |_   _ __    ___   _ __     __ _  | |_  | |__  
+ \___ \  | __| | '__|  / _ \ | '_ \   / _` | | __| | '_ \ 
+  ___) | | |_  | |    |  __/ | | | | | (_| | | |_  | | | |
+ |____/   \__| |_|     \___| |_| |_|  \__, |  \__| |_| |_|
+                                      |___/               
+==========================================================
+       LOW: 1   MEDIUM: 2   HIGH: 3    MAX: 4
+==========================================================
+    Pick strength (1-4):
     """.trimIndent()
         )
     }
 
     override fun process(order: Order): Order {
-        val value = readln().toInt()
-        val strength = Strength.get(value)
-        println("Strength selected: $strength")
-        order.strength = strength
+        val inputStrength = readln().toInt()
+        if (inputStrength in 1..4) {
+            val selectedStrength = Strength.get(inputStrength)
+            println("Strength selected: $selectedStrength")
+            order.strength = selectedStrength
+        } else {
+            println("Strength unsupported: $inputStrength")
+            this.print()
+            this.process(order)
+        }
         return order
     }
 }
