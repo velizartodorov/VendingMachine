724c1e0ce10f877f034839bc521f45b1389a4700
velizartodorov <willking66@gmail.com>
2025-03-08 13:08:35 +0100

    :hammer: refactor `State` class

--- src/main/kotlin/vending_machine/State.kt
+++ src/main/kotlin/vending_machine/State.kt
@@ -1,8 +1,8 @@
 package vending_machine
 
 sealed class State {
-    object Idle : State()
-    object Running : State()
-    object Paused : State()
-    object Stopped : State()
+    data object Idle : State()
+    data object Running : State()
+    data object Paused : State()
+    data object Stopped : State()
 }
